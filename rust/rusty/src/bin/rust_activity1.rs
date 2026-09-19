1427ca89aa0722967427539f3acb5eb20c144369
malemanape <dcruzsteven1@gmail.com>
2026-09-19 15:57:04 +0800

    sum of stuffs

--- rust/rusty/src/bin/rust_activity1.rs
+++ rust/rusty/src/bin/rust_activity1.rs
@@ -1,1 +1,30 @@
-// I start here laterrr hehehezz.
+use std::io::{self, Write};
+
+fn sum_of_digits(mut number: u64) -> u64 {
+    let mut sum = 0;
+
+    while number > 0 {
+        sum += number % 10;
+        number /= 10;
+    }
+    sum
+}
+
+fn main() {
+    print!("Enter a postive integer: ");
+    io::stdout().flush().unwrap();
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+
+    match input.trim().parse::<u64>() {
+        Ok(number) => {
+            let total_sum = sum_of_digits(number);
+            println!("Original number: {}", number);
+            println!("Sum of all digits: {}", total_sum);
+        }
+        Err(_) => {
+            println!("Error: Input must be a positive integer! Your input: {}", input);
+        }
+    }
+}
